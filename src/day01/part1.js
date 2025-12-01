a3e766b97a11d94828bfc8c8673cbb7f7b2e3915
Tony McNulty <toneee@gmail.com>
2025-12-01 20:56:49 +0000

    Day 1: Secret entrance (part 1)

--- src/day01/part1.js
+++ src/day01/part1.js
@@ -1,3 +1,26 @@
 export function solve(input) {
-    return { 'answer': 0 }
+    const lines = input.split("\n")
+
+    let zero_count = 0
+    let pos = 50
+    for (const line of lines) {
+        pos = rotate(pos, line[0], +line.slice(1))
+
+        if (pos == 0) {
+            zero_count++
+        }
+    }
+    return { 'answer': zero_count }
+}
+
+function rotate(pos, direction, clicks) {
+    if (direction == 'R') {
+        pos = (pos + clicks) % 100
+    } else {
+        pos = (pos - clicks) % 100
+    }
+    if (pos < 0) {
+        pos += 100
+    }
+    return pos
 }
